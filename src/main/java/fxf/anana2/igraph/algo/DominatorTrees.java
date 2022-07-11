cb1b7899643e2f085425138604c34c6a916e62af
anana2 <22158218+anana2@users.noreply.github.com>
2022-07-11 13:20:37 -0400

    Update DominatorTrees.java

--- src/main/java/fxf/anana2/igraph/algo/DominatorTrees.java
+++ src/main/java/fxf/anana2/igraph/algo/DominatorTrees.java
@@ -2,10 +2,9 @@ package fxf.anana2.igraph.algo;
 
 import fxf.anana2.igraph.FlowGraph;
 import fxf.anana2.igraph.ParentTree;
-import fxf.anana2.igraph.ReverseFlowGraph;
 
 public interface DominatorTrees extends ParentTree {
-    public static <G extends FlowGraph & ReverseFlowGraph> ParentTree snca(G graph) {
+    public static ParentTree snca(FlowGraph graph) {
         return SNCADominatorTree.from(graph);
     }
 }
